d8d9b252ce46f3b4d2eb2a9ed337cfef608635d8
David Markovitz <dumarkov@microsoft.com>
2020-06-16 19:34:46 +0300

    Could it be?!

---
+++ OrderFileKeeper/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import azure.functions as func
+import json, requests, os
+from pydocumentdb import document_client
+
+def main(event: func.EventHubEvent):
+
+    cosmosdb_order_masterKey = os.environ.get('cosmosdb_order_masterKey')
+    cosmosdb_order_host = os.environ.get('cosmosdb_order_host')
+    cosmos_db_order_databaseId = os.environ.get('cosmos_db_order_databaseId')
+    cosmos_db_order_collectionId = os.environ.get('cosmos_db_order_collectionId')    
+
+    event_body = event.get_body().decode('utf-8')
+    logging.info(event_body)
+
+    combineOrderContent = os.environ.get('combineOrderContent')    
+
+    response = requests.post(combineOrderContent, data = event_body)
+    doc_json = response.json()
+
+    client = document_client.DocumentClient(cosmosdb_order_host, {'masterKey': cosmosdb_order_masterKey})
+
+    for sale in doc_json:
+
+        dbLink = 'dbs/' + cosmos_db_order_databaseId
+        collLink = dbLink + '/colls/' + cosmos_db_order_collectionId
+
+        sale['salesNumber'] = sale['headers']['salesNumber']
+
+        client.CreateDocument(collLink, sale)
+
+
---
+++ OrderFileSeeker/__init__.py
@@ -0,0 +1,59 @@
+import logging
+import azure.functions as func
+import os, json, re
+from azure.storage.blob import ContainerClient
+from azure.eventhub import EventHubProducerClient, EventData
+
+order_file_type = {
+     'orderheaderdetails': 'orderHeaderDetailsCSVUrl'
+    ,'orderlineitems': 'orderLineItemsCSVUrl'
+    ,'productinformation': 'productInformationCSVUrl'
+    }
+
+def main(event: func.EventHubEvent):
+
+    storage_orders = os.environ.get('storage_orders')
+    storage_orders_container = os.environ.get('storage_orders_container')
+    eventhub_ns_sap_sl = os.environ.get('eventhub_ns_sap_sl')
+    eventhub_order_combine_files = os.environ.get('eventhub_order_combine_files')
+    
+    container = ContainerClient.from_connection_string(conn_str = storage_orders, container_name = storage_orders_container)
+
+    for e in event:
+
+        event_body = e.get_body().decode('utf-8')
+        
+        logging.info(event_body)
+
+        event_json = json.loads(event_body)[0]
+    
+        url = event_json["data"]["url"]
+        url_dirname = os.path.dirname(url)
+        url_basename = os.path.basename(url)
+        order_id = re.findall('\d+', url_basename)[0]
+
+        blobs = list(container.list_blobs(name_starts_with = order_id))
+        
+        blob_ts = {b['name'] : (b['last_modified'], b['etag'])  for b in blobs}
+        blob_ts_max = max(blob_ts.values())
+        
+        doc = dict()
+
+        if len(blobs) == 3 and blob_ts[url_basename] == blob_ts_max:
+            for b in blobs:
+                blob_name = b['name']
+                file_type = order_file_type.get(re.findall('(?<=-)\w+(?=\.)', blob_name)[0].lower())
+
+                doc[file_type] = f'{url_dirname}/{blob_name}'
+            
+            doc_json = json.dumps(doc)
+            
+            producer = EventHubProducerClient.from_connection_string(conn_str = eventhub_ns_sap_sl, eventhub_name = eventhub_order_combine_files)
+
+            try: 
+                event_data_batch = producer.create_batch()
+                event_data_batch.add(EventData(doc_json))         
+                producer.send_batch(event_data_batch)
+            finally:
+                producer.close()
+
