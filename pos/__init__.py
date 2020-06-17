20e2bbee17337455fd27442f8e3a4b9352657406
David Markovitz <dumarkov@microsoft.com>
2020-06-17 11:55:36 +0300

    servicebus

--- pos/__init__.py
+++ pos/__init__.py
@@ -2,7 +2,7 @@ import logging
 import azure.functions as func
 import json, requests, os
 from pydocumentdb import document_client
-
+from azure.servicebus import TopicClient, Message
 
 def main(event: func.EventHubEvent):
 
@@ -10,6 +10,10 @@ def main(event: func.EventHubEvent):
     cosmosdb_pos_host = os.environ.get('cosmosdb_pos_host')
     cosmosdb_pos_databaseId = os.environ.get('cosmosdb_pos_databaseId')
     cosmosdb_pos_collectionId = os.environ.get('cosmosdb_pos_collectionId')    
+
+    servicebus_pos_conn_str = os.environ.get('servicebus_pos_conn_str')
+    servicebus_pos_topic_name = os.environ.get('servicebus_pos_topic_name')
+
 
     dbLink = 'dbs/' + cosmosdb_pos_databaseId
     collLink = dbLink + '/colls/' + cosmosdb_pos_collectionId
@@ -25,3 +29,18 @@ def main(event: func.EventHubEvent):
         e['id'] = e['header']['salesNumber']
 
         client.CreateDocument(collLink, e)
+
+
+        receipt = {
+            "totalItems": len(e["details"]),
+            "totalCost": e["header"]["totalCost"],
+            "salesNumber": e["header"]["salesNumber"],
+            "salesDate": e["header"]["dateTime"],
+            "storeLocation": e["header"]["locationId"],
+            "receiptUrl": e["header"]["receiptUrl"]
+        }
+
+        tc = TopicClient.from_connection_string(servicebus_pos_conn_str, servicebus_pos_topic_name)
+        msg = Message(json.dumps(receipt).encode('utf8'))
+        tc.send(msg)
+
