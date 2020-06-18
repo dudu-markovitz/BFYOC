796d7e22b1c19d5a611c79dfcfb460f9588e771d
David Markovitz <dumarkov@microsoft.com>
2020-06-18 15:29:35 +0300

    cosmosdb_changefeed_rating

---
+++ cosmosdb_changefeed_rating/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import azure.functions as func
+from azure.eventhub import EventHubProducerClient, EventData
+import os, json
+
+def main(documents: func.DocumentList) -> str:
+
+    eventhub_ns_sap_sl = os.environ.get('eventhub_ns_sap_sl')
+    eventhub_cosmosdb_change_feed = os.environ.get('eventhub_cosmosdb_change_feed')
+
+    producer = EventHubProducerClient.from_connection_string(conn_str = eventhub_ns_sap_sl, eventhub_name = eventhub_cosmosdb_change_feed)
+    event_data_batch = producer.create_batch()
+
+    for doc in documents:
+        event_data_batch.add(EventData(json.dumps(doc)))     
+
+    try: 
+        producer.send_batch(event_data_batch)
+    finally:
+        producer.close()
